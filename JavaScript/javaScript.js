924886ef5bb8ba97580f1d22d928c3bd687f0928
GitHubProo <42809244+GitHubProo@users.noreply.github.com>
2019-10-28 13:18:02 +0300

    lesson 7

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,11 @@
+function index() {
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+      document.write("number:" + i + "<br>");
+    } else {
+      document.write("number:" + i + "<br>");
+    }
+  }
+}
+
+index();
