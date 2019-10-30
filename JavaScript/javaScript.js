a5da9569f6a023959c979855dccbe5d637145973
volodymyr.zhyhaylo <volodymyr.zhyhaylo@gmail.com>
2019-10-30 19:44:14 +0200

    array push

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -1,6 +1,6 @@
 var days = document.getElementById("selectDay");
 var textArea = document.getElementById("textAreaId");
- 
+
 var arr = [];
 
 function btnSend() {
@@ -14,19 +14,13 @@ function btnSend() {
 }
 
 function btnPrint() {
-    var ob = {
-        day: days.value,
-        note: textArea.value 
-      };
-      
-      document.getElementById("textPrint2").value = ob.note
-      
-      console.log(ob.note);
-  //var days = document.getElementById("selectDay").value;
-  //console.log(days);
-  //var textArea = document.getElementById("textArea").value;
-  //var textArea = document.getElementById("textPrint2").value = textAreaId;
-  
-    //textArea.value = textArea
-    //console.log(textArea);
+  var ob = {
+    day: days.value,
+    note: textArea.value
+  };
+  arr.push(ob);
+  console.log(ob.note);
+  console.log(arr);
+  document.getElementById("textPrint1").value = ob.day;
+  document.getElementById("textPrint2").value = ob.note;
 }
